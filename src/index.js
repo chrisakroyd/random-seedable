3266185febd6873e1186dba63f8e6c2061c3507b
Christopher Akroyd <chris@chrisakroyd.com>
2021-05-07 18:41:04 +0100

    Public api.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export { default as LCG } from './lcg.js';
+export { default as PCG } from './pcg.js';
+export { default as MersenneTwister } from './mersenneTwister.js';
+export { default as XORShift } from './xorshift.js';
+export { default as XORWow } from './xorwow.js';
+
+export const shuffle = (array, inplace) => {
+};
