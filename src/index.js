5589424f11d01888dd0636cb0c3fc1a9938aee68
Christopher Akroyd <chris@chrisakroyd.com>
2021-05-19 16:00:27 +0100

    Exposed a default random option.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import XorWow from './xorwow.js';
+
 export { default as LCG } from './lcg.js';
 export { default as PCG } from './pcg.js';
 export { default as MersenneTwister } from './mersenneTwister.js';
 export { default as XORShift } from './xorshift.js';
 export { default as XORWow } from './xorwow.js';
 
-export const shuffle = (array, inplace) => {
-};
+export const random = new XorWow(123456789);
+export default new XorWow(123456789);
